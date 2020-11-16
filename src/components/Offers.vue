5a777624de1cd13327a8a838eaed987bfe57b333
qiiwen <qiiwen@users.noreply.github.com>
2020-11-16 13:45:57 +0800

    scoped style

--- src/components/Offers.vue
+++ src/components/Offers.vue
@@ -256,15 +256,6 @@ export default {
     setTimeout(function () {
       console.log("mounted()");
     }, 3000);
-    if (localStorage.getItem('reloaded')) {
-        // The page was just reloaded. Clear the value from local storage
-        // so that it will reload the next time this page is visited.
-        localStorage.removeItem('reloaded');
-    } else {
-        // Set a flag so that we know not to reload the page twice.
-        localStorage.setItem('reloaded', '1');
-        location.reload();
-    }
   },
   beforeUpdate: function () {
     console.log("beforeUpdate()");
--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -54,7 +54,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 ul{
   display: flex;
   flex-wrap:wrap;
